e52aaf961c3c97ccca30231795e9bf3ba3927079
Your Name <sjukic1@etf.unsa.ba>
2020-11-28 18:00:21 +0100

    testovi

---
+++ Domari/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domari;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+        }
+    }
+}
