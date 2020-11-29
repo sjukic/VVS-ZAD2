85c9f44853ccf6a0fc30b4b543d792e5fd4a22bf
Your Name <sjukic1@etf.unsa.ba>
2020-11-29 14:54:59 +0100

    sda

--- Domari/UnitTestProject1/PromjenaSobeTest.cs
+++ Domari/UnitTestProject1/PromjenaSobeTest.cs
@@ -16,6 +16,8 @@ namespace UnitTestProject1
             studentskiDom.Studenti.Add(student1);
             studentskiDom.Studenti.Add(student2);
             Soba soba = studentskiDom.Sobe[0];
+            soba.Stanari.Add(student1);
+            soba.Stanari.Add(student2);
             studentskiDom.PromjenaSobe(soba, 3);
             Assert.AreEqual(3, soba.Kapacitet);
         }
@@ -23,14 +25,16 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestKadaJeKapacitetIsti()
         {
-            int brojSobe = 250;
+            int brojSobe = 350;
             Student student1 = new Student(), student2 = new Student();
             StudentskiDom studentskiDom = new StudentskiDom(brojSobe);
             studentskiDom.Studenti.Add(student1);
             studentskiDom.Studenti.Add(student2);
             Soba soba = studentskiDom.Sobe[0];
-            studentskiDom.PromjenaSobe(soba, 3);
-            Assert.AreEqual(3, soba.Kapacitet);
+            soba.Stanari.Add(student1);
+            soba.Stanari.Add(student2);
+            studentskiDom.PromjenaSobe(soba, 2);
+            Assert.AreEqual(2, soba.Kapacitet);
         }
 
         [TestMethod]
@@ -42,8 +46,10 @@ namespace UnitTestProject1
             studentskiDom.Studenti.Add(student1);
             studentskiDom.Studenti.Add(student2);
             Soba soba = studentskiDom.Sobe[0];
-            studentskiDom.PromjenaSobe(soba, 3);
-            Assert.AreEqual(3, soba.Kapacitet);
+            soba.Stanari.Add(student1);
+            soba.Stanari.Add(student2);
+            studentskiDom.PromjenaSobe(soba, 1);
+            Assert.AreEqual(1, soba.Kapacitet);
         }
     }
 }
