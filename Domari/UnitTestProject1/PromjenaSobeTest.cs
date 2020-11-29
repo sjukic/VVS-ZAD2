109027e315d174cff39d8dbe2d71ab55515d0622
Your Name <sjukic1@etf.unsa.ba>
2020-11-29 14:32:30 +0100

    s

--- Domari/UnitTestProject1/PromjenaSobeTest.cs
+++ Domari/UnitTestProject1/PromjenaSobeTest.cs
@@ -8,9 +8,22 @@ namespace UnitTestProject1
     public class PromjenaSobeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestKadaJeKapacitetVeci()
         {
-            int brojSobe=122, kapacitet=2;
+            int brojSobe = 122;
+            Student student1 = new Student(), student2 = new Student();
+            StudentskiDom studentskiDom = new StudentskiDom(brojSobe);
+            studentskiDom.Studenti.Add(student1);
+            studentskiDom.Studenti.Add(student2);
+            Soba soba = studentskiDom.Sobe[0];
+            studentskiDom.PromjenaSobe(soba, 3);
+            Assert.AreEqual(3, soba.Kapacitet);
+        }
+
+        [TestMethod]
+        public void TestKadaJeKapacitetIsti()
+        {
+            int brojSobe = 250;
             Student student1 = new Student(), student2 = new Student();
             StudentskiDom studentskiDom = new StudentskiDom(brojSobe);
             studentskiDom.Studenti.Add(student1);
